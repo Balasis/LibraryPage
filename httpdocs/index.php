fa1f3d93116afc2f90ffd6f9e00dc0ef81afa816
Balasis <giovani1994a@gmail.com>
2023-03-19 04:01:11 +0200

    HTML basics

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -3,12 +3,18 @@ require "../CS/CS.php";
 ?>
 
 <!DOCTYPE html>
+<html>
 <head>
-<style>
-.circle{
-}
-</style>
+<title>My Code Library</title>
+<meta name="author" content="John Balasis" />
+<meta name="viewport" content="width=device-width , initial-scale=1"/>
+<meta http-equiv="refresh" content="9000"/>
+<link rel="stylesheet" src="simple.css" type="text/css"/>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 </head>
 <body>
+<main>
+
+</main>
 </body>
 </html>
