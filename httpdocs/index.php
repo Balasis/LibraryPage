934289549113bdcae9630cd7f7b40881dadc8e80
Balasis <giovani1994a@gmail.com>
2023-03-19 08:53:55 +0200

    little light travels along css

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -1,20 +1,24 @@
-<?php
-require "../CS/CS.php";
-?>
+
 
 <!DOCTYPE html>
 <html>
 <head>
+    <style></style>
 <title>My Code Library</title>
+<link rel="stylesheet" href="simple.css" type="text/css"/>
+
 <meta name="author" content="John Balasis" />
+
 <meta name="viewport" content="width=device-width , initial-scale=1"/>
 <meta http-equiv="refresh" content="9000"/>
-<link rel="stylesheet" src="simple.css" type="text/css"/>
+<meta charset="UTF-8" />
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 </head>
 <body>
-<main>
+    <main>
 <div id="masterBox" >
+    <div id="light"></div>
 <div id="upperSearch">
 </div>
 <div id="lowerSearch">
