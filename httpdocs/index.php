db54bb5beb00324a88e216d9ebf9e9c9ab2eccc6
Balasis <giovani1994a@gmail.com>
2023-03-19 04:48:18 +0200

    added some css to start

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -14,7 +14,29 @@ require "../CS/CS.php";
 </head>
 <body>
 <main>
+<div id="masterBox" >
+<div id="upperSearch">
+</div>
+<div id="lowerSearch">
 
+    <div id="optionTab">
+        <div id="search">
+        </div>
+        <div id="list">
+        </div>
+    </div>
+
+    <div id="viewTab">
+        <div id="description">
+        </div>
+        <div id="pictures">
+        </div>
+    </div>
+
+</div>    
+
+
+</div>
 </main>
 </body>
 </html>
