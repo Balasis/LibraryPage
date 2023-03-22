b4201321b1cb431399924eb2df91aca0f8469eb2
Balasis <giovani1994a@gmail.com>
2023-03-22 22:24:03 +0200

    prevent sql  injection whitelist Category

--- httpdocs/listReq.php
+++ httpdocs/listReq.php
@@ -1,18 +1,34 @@
 <?php
 require_once "../CS/Cs.php";
 $Categ=$_GET['q'];
-$Fetchquer = "SELECT tagName FROM $Categ";
-$Fetchque= sqlsrv_query($conn,$Fetchquer);
+
+ $whitelist=sqlsrv_query($conn,"SELECT name FROM sys.tables");
+
+ $DaWhiteList=array();
+
+ while($whitelistt=sqlsrv_fetch_array($whitelist,SQLSRV_FETCH_ASSOC)){
+    array_push($DaWhiteList,$whitelistt['name']);
+ };
+ if (in_array($Categ,$DaWhiteList)){
+    $Fetchquer = "SELECT tagName FROM $Categ";
+    $Fetchque= sqlsrv_query($conn,$Fetchquer);
+    
+    
+    echo "<li id='ChosenCat'>".$Categ."</li>";
+    while($Fetchquerr=sqlsrv_fetch_array($Fetchque,SQLSRV_FETCH_ASSOC)){
+        echo "<li>";
+        echo "<a class='listLi' id='";
+        echo htmlspecialchars($Fetchquerr['tagName'],ENT_QUOTES);
+        echo "'".'onclick="getCodeInfo(this.id)">'.htmlspecialchars($Fetchquerr['tagName'],ENT_QUOTES)."</a>";
+        echo "</li>";
+    }
+
+ }else{
+
+    echo "haha jokes on you injector";
+ }
 
 
-echo "<li id='ChosenCat'>".$Categ."</li>";
-while($Fetchquerr=sqlsrv_fetch_array($Fetchque,SQLSRV_FETCH_ASSOC)){
-    echo "<li>";
-    echo "<a class='listLi' id='";
-    echo htmlspecialchars($Fetchquerr['tagName'],ENT_QUOTES);
-    echo "'".'onclick="getCodeInfo(this.id)">'.htmlspecialchars($Fetchquerr['tagName'],ENT_QUOTES)."</a>";
-    echo "</li>";
-}
 
 
 ?>
